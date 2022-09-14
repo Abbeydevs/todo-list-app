52811da7cdee5f08ab673e9b1e8bdf867c37f7f4
Abbeydevs <abbey.developer@gmail.com>
2022-09-14 10:27:19 +0100

    Delete a function

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,10 @@ const Form = () => {
 
     //Things I need to do here
     //Get Input value
-    const getInputValue = (e) => {
-        console.log(e.target.value)
+    // const getInputValue = (e) => {
+    //     console.log(e.target.value
         
-    }
+    // }
 
     //Submit the form
     const handleSubmit = (e) => {
